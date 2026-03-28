17b607bbbc2058933ed3204292febc686d043e43
Denis Lebedev <829783+delebedev@users.noreply.github.com>
2026-03-28 09:04:29 +0100

    style: fmt

--- matchdoor/src/test/kotlin/leyline/conformance/ConcealingCurtainsPuzzleTest.kt
+++ matchdoor/src/test/kotlin/leyline/conformance/ConcealingCurtainsPuzzleTest.kt
@@ -4,16 +4,17 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import leyline.ConformanceTag
 
-class ConcealingCurtainsPuzzleTest : FunSpec({
+class ConcealingCurtainsPuzzleTest :
+    FunSpec({
 
-    tags(ConformanceTag)
+        tags(ConformanceTag)
 
-    val base = ConformanceTestBase()
-    beforeSpec { base.initCardDatabase() }
-    afterEach { base.tearDown() }
+        val base = ConformanceTestBase()
+        beforeSpec { base.initCardDatabase() }
+        afterEach { base.tearDown() }
 
-    test("concealing curtains transform puzzle loads and starts") {
-        val pzl = """
+        test("concealing curtains transform puzzle loads and starts") {
+            val pzl = """
             [metadata]
             Name:Concealing Curtains Transform
             Goal:Win
@@ -32,10 +33,10 @@ class ConcealingCurtainsPuzzleTest : FunSpec({
             aibattlefield=
             aihand=Grizzly Bears
             ailibrary=Forest
-        """.trimIndent()
-        val (b, game, _) = base.startPuzzleAtMain1(pzl)
-        val bf = game.humanPlayer.getZone(forge.game.zone.ZoneType.Battlefield).cards
-        bf.any { it.name == "Concealing Curtains" } shouldBe true
-        bf.count { it.isLand } shouldBe 3
-    }
-})
+            """.trimIndent()
+            val (b, game, _) = base.startPuzzleAtMain1(pzl)
+            val bf = game.humanPlayer.getZone(forge.game.zone.ZoneType.Battlefield).cards
+            bf.any { it.name == "Concealing Curtains" } shouldBe true
+            bf.count { it.isLand } shouldBe 3
+        }
+    })
