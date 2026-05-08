bb42a6eacbed94d61fc481c5076d5b97996c9751
Denis Lebedev <829783+delebedev@users.noreply.github.com>
2026-05-08 19:08:23 +0100

    matchdoor: add impending hand alt-cost rail

---
+++ matchdoor/src/test/kotlin/leyline/mechanics/impending/ImpendingLifecycleTest.kt
@@ -0,0 +1,89 @@
+package leyline.mechanics.impending
+
+import forge.game.card.CounterEnumType
+import forge.game.zone.ZoneType
+import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+import leyline.game.data.KeywordAbilityIds
+import leyline.testkit.SessionTest
+import leyline.testkit.detailInt
+import leyline.testkit.detailString
+import leyline.testkit.persistentAnnotationsOfType
+import wotc.mtgo.gre.external.messaging.Messages.AnnotationType
+import wotc.mtgo.gre.external.messaging.Messages.CastingTimeOptionType
+
+private val PUZZLE =
+    """
+    [metadata]
+    Name:Impending Overlord of the Mistmoors
+    Goal:Cast Overlord of the Mistmoors for its impending cost.
+    Turns:6
+    Difficulty:Easy
+
+    [state]
+    ActivePlayer=Human
+    ActivePhase=Main1
+    HumanLife=20
+    AILife=20
+
+    humanhand=Overlord of the Mistmoors
+    humanbattlefield=Plains;Plains;Plains;Plains
+    humanlibrary=Plains;Plains;Plains;Plains;Plains;Plains;Plains;Plains
+    ailibrary=Mountain;Mountain;Mountain;Mountain;Mountain;Mountain;Mountain;Mountain
+    """.trimIndent()
+
+class ImpendingLifecycleTest :
+    SessionTest({
+        test("impending cast enters with time counters and records cast-through option") {
+            startPuzzleRaw(PUZZLE)
+            val overlordGrpId = harness.bridge.cardRepository.findGrpIdByName("Overlord of the Mistmoors")!!
+            val impendingAbilityGrpId =
+                harness.bridge.cardRepository.findKeywordAbilityGrpId(overlordGrpId, KeywordAbilityIds.IMPENDING)!!
+
+            val snap = messageSnapshot()
+            castSpellByName("Overlord of the Mistmoors", alternativeGrpId = impendingAbilityGrpId).shouldBeTrue()
+            passUntilResolved(maxPasses = 12)
+
+            val cto =
+                messagesSince(snap)
+                    .persistentAnnotationsOfType(AnnotationType.CastingTimeOption)
+                    .first { it.detailInt("alternateCostGrpId") == impendingAbilityGrpId }
+            val overlord = human.getZone(ZoneType.Battlefield).cards.first { it.name == "Overlord of the Mistmoors" }
+            val counterRemoved =
+                messagesSince(snap)
+                    .flatMap { it.gameStateMessage.annotationsList }
+                    .first { AnnotationType.CounterRemoved in it.typeList }
+
+            assertSoftly {
+                cto.detailInt("type") shouldBe CastingTimeOptionType.CastThroughAbility.number
+                overlord.getCounters(CounterEnumType.TIME) shouldBe 3
+                overlord.isCreature.shouldBeFalse()
+                counterRemoved.detailString("counter_type") shouldBe "Time"
+                counterRemoved.detailInt("transaction_amount") shouldBe 1
+            }
+        }
+
+        test("impending first end-step trigger removes one time counter") {
+            startPuzzleRaw(PUZZLE)
+            val overlordGrpId = harness.bridge.cardRepository.findGrpIdByName("Overlord of the Mistmoors")!!
+            val impendingAbilityGrpId =
+                harness.bridge.cardRepository.findKeywordAbilityGrpId(overlordGrpId, KeywordAbilityIds.IMPENDING)!!
+
+            castSpellByName("Overlord of the Mistmoors", alternativeGrpId = impendingAbilityGrpId).shouldBeTrue()
+            passUntilResolved(maxPasses = 12)
+
+            val removed =
+                allMessages
+                    .flatMap { it.gameStateMessage.annotationsList }
+                    .filter { AnnotationType.CounterRemoved in it.typeList }
+
+            val overlord = human.getZone(ZoneType.Battlefield).cards.first { it.name == "Overlord of the Mistmoors" }
+            assertSoftly {
+                removed.any { it.detailString("counter_type") == "Time" && it.detailInt("transaction_amount") == 1 }.shouldBeTrue()
+                overlord.getCounters(CounterEnumType.TIME) shouldBe 3
+                overlord.isCreature.shouldBeFalse()
+            }
+        }
+    })
