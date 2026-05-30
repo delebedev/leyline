f7f9bb595e3038a7b816281889b1efcf5b18b2f4
Denis Lebedev <829783+delebedev@users.noreply.github.com>
2026-05-30 17:30:22 +0100

    test(matchdoor): cover banner choice annotations

--- matchdoor/src/test/kotlin/leyline/behavior/cards/BannerStaticChoiceTest.kt
+++ matchdoor/src/test/kotlin/leyline/behavior/cards/BannerStaticChoiceTest.kt
@@ -4,8 +4,13 @@ import io.kotest.assertions.assertSoftly
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.ints.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
+import leyline.game.codes.DetailKeys
 import leyline.game.codes.StaticChoiceIds
 import leyline.testkit.SessionTest
+import leyline.testkit.detailInt
+import leyline.testkit.detailString
+import leyline.testkit.lastGsmMatching
+import wotc.mtgo.gre.external.messaging.Messages.AnnotationType
 import wotc.mtgo.gre.external.messaging.Messages.SelectionListType
 import wotc.mtgo.gre.external.messaging.Messages.StaticList
 
@@ -25,6 +30,74 @@ class BannerStaticChoiceTest :
                 ids shouldContain StaticChoiceIds.subtypeIdFor("Berserker")!!
                 ids shouldContain StaticChoiceIds.subtypeIdFor("Human")!!
                 ids shouldContain StaticChoiceIds.subtypeIdFor("Kithkin")!!
+            }
+        }
+
+        test("Patchwork Banner static subtype choice emits ChoiceResult and LinkInfo") {
+            startPuzzleFile("puzzles/patchwork-banner-static-choice.pzl", validating = true)
+
+            castSpellUntilSelectNReq("Patchwork Banner")
+            val goblinId = StaticChoiceIds.subtypeIdFor("Goblin")!!
+            respondToSelectN(listOf(goblinId))
+            passUntilResolved()
+
+            val choiceResult =
+                checkNotNull(
+                    allMessages.lastGsmMatching { gsm ->
+                        gsm.annotationsList.any { AnnotationType.ChoiceResult in it.typeList }
+                    },
+                ) { "No ChoiceResult annotation emitted" }
+                    .annotationsList
+                    .first { AnnotationType.ChoiceResult in it.typeList }
+            val linkInfo =
+                checkNotNull(
+                    allMessages.lastGsmMatching { gsm ->
+                        gsm.persistentAnnotationsList.any { AnnotationType.LinkInfo in it.typeList }
+                    },
+                ) { "No persistent LinkInfo annotation emitted" }
+                    .persistentAnnotationsList
+                    .first { AnnotationType.LinkInfo in it.typeList }
+
+            assertSoftly {
+                choiceResult.detailInt(DetailKeys.CHOICE_VALUE) shouldBe goblinId
+                choiceResult.detailInt(DetailKeys.CHOICE_DOMAIN) shouldBe 5
+                linkInfo.affectedIdsList shouldBe listOf(6, goblinId)
+                linkInfo.detailString(DetailKeys.CHOOSE_LINK_TYPE) shouldBe "Type"
+                linkInfo.detailInt(DetailKeys.SOURCE_ABILITY_GRPID) shouldBe 176647
+            }
+        }
+
+        test("Heraldic Banner static color choice emits ChoiceResult and LinkInfo") {
+            startPuzzleFile("puzzles/heraldic-banner-static-choice.pzl", validating = true)
+
+            castSpellUntilSelectNReq("Heraldic Banner")
+            val redId = StaticChoiceIds.colorIdForName("Red")!!
+            respondToSelectN(listOf(redId))
+            passUntilResolved()
+
+            val choiceResult =
+                checkNotNull(
+                    allMessages.lastGsmMatching { gsm ->
+                        gsm.annotationsList.any { AnnotationType.ChoiceResult in it.typeList }
+                    },
+                ) { "No ChoiceResult annotation emitted" }
+                    .annotationsList
+                    .first { AnnotationType.ChoiceResult in it.typeList }
+            val linkInfo =
+                checkNotNull(
+                    allMessages.lastGsmMatching { gsm ->
+                        gsm.persistentAnnotationsList.any { AnnotationType.LinkInfo in it.typeList }
+                    },
+                ) { "No persistent LinkInfo annotation emitted" }
+                    .persistentAnnotationsList
+                    .first { AnnotationType.LinkInfo in it.typeList }
+
+            assertSoftly {
+                choiceResult.detailInt(DetailKeys.CHOICE_VALUE) shouldBe redId
+                choiceResult.detailInt(DetailKeys.CHOICE_DOMAIN) shouldBe 6
+                linkInfo.affectedIdsList shouldBe listOf(redId)
+                linkInfo.detailString(DetailKeys.CHOOSE_LINK_TYPE) shouldBe "Color"
+                linkInfo.detailInt(DetailKeys.SOURCE_ABILITY_GRPID) shouldBe 88237
             }
         }
     })
